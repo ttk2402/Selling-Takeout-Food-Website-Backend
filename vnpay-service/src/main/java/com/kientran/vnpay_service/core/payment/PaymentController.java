52e1cb19c1832b1472d4919659ae92c738a9eb32
ttk2402 <trantrungkien2402@gmail.com>
2024-10-11 05:23:48 +0700

    add vnpay service to payment for website

---
+++ vnpay-service/src/main/java/com/kientran/vnpay_service/core/payment/PaymentController.java
@@ -0,0 +1,29 @@
+package com.kientran.vnpay_service.core.payment;
+
+import com.kientran.vnpay_service.core.response.ResponseObject;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${spring.application.api-prefix}/payment")
+@RequiredArgsConstructor
+public class PaymentController {
+    private final PaymentService paymentService;
+    @GetMapping("/vn-pay")
+    public ResponseObject<PaymentDTO.VNPayResponse> pay(HttpServletRequest request) {
+        return new ResponseObject<>(HttpStatus.OK, "Success", paymentService.createVnPayPayment(request));
+    }
+    @GetMapping("/vn-pay-callback")
+    public ResponseObject<PaymentDTO.VNPayResponse> payCallbackHandler(HttpServletRequest request) {
+        String status = request.getParameter("vnp_ResponseCode");
+        if (status.equals("00")) {
+            return new ResponseObject<>(HttpStatus.OK, "Success", new PaymentDTO.VNPayResponse("00", "Success", ""));
+        } else {
+            return new ResponseObject<>(HttpStatus.BAD_REQUEST, "Failed", null);
+        }
+    }
+}
